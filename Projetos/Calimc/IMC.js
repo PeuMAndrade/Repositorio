2735893bcce61df1a6125e23c9d1bd8fa314f5df
Pedro Henrique Mascarenhas de Andrade <peumandrade@gmail.com>
2023-08-12 20:10:56 -0300

    Projeto finalizado

--- Projetos/Calimc/IMC.js
+++ Projetos/Calimc/IMC.js
@@ -1,22 +1,27 @@
+
 function calcular(){
     let pesotxt = document.querySelector('#peso')
     let alturatxt = document.querySelector('#altura')
     let peso = Number(pesotxt.value)
-    let altura = Number(alturatxt.value)
-        if(peso == 0 || altura == 0){
+    let altura = Number(alturatxt.value.replace(",","."))
+    let imc = 0
+        if(peso == 0 || altura == 0 || altura > 2.5){
             alert(`Você não informou todos os dados`)
         } else{
-            let imc = peso/(altura**2)
-            let = imc
-            res.innerHTML = `IMC: ${imc.toFixed(1)}<br>`
+            imc = peso/(altura**2)
+            if(isNaN(imc)){
+                alert(`Você não informou todos os dados ou informou incorretamente`)
+            } else{
+            res.innerHTML = ''
+            res.innerHTML += `<br>IMC= ${imc.toFixed(1)}<br>`
             if(imc < 18.5){
-                res.innerHTML += `BAIXO PESO`
+                res.innerHTML += `BAIXO PESO <br>`
             } else if(imc >= 18.5 || imc < 24.99){
-                res.innerHTML += `NORMAL`
+                res.innerHTML += `NORMAL <br>`
             } else if(imc >= 25 || imc < 29.99){
-                res.innerHTML += `SOBREPESO`
+                res.innerHTML += `SOBREPESO <br>`
             } else if(imc >= 30){
-                res.innerHTML += `OBESIDADE`
-    }
+                res.innerHTML += `OBESIDADE <br>`
+    }}
 }
 }
